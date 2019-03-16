6b87ac80da21086437dd7972eed756bb26d892e1
Andrew <odwalda@gmail.com>
2019-03-16 19:39:04 +0000

    Update SkullInteractionScript.cs

--- Assets/Scripts/InteractableObjects/SkullInteractionScript.cs
+++ Assets/Scripts/InteractableObjects/SkullInteractionScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SkullInteractionScript : InteractiveObject
 {
@@ -15,7 +16,13 @@ public class SkullInteractionScript : InteractiveObject
         // Display the text from the skull
         gameObject.GetComponent<subTitleTrigger>().StartDisplay();
 
-        // Remove the item from the world
-        Destroy(gameObject);
+        // Remove the item from the world       
+        for(var i = 0; i < transform.childCount; i++)
+        {
+            if(transform.GetChild(i).GetComponent<Renderer>() != null)
+                transform.GetChild(i).GetComponent<Renderer>().enabled = false;
+        }
+
+        enabled = false;
     }
 }
