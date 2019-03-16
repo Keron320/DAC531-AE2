ef04210ca3319a23d1bc764b8184537f92b537e4
Andrew <odwalda@gmail.com>
2019-03-16 19:18:58 +0000

    Skull interaction in world

---
+++ Assets/Scripts/InteractableObjects/SkullInteractionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullInteractionScript : InteractiveObject
+{
+    public GameObject subtitleTrigger;
+
+    public override void UseItem()
+    {
+        // Add the item
+        var inventoryManager = GameObject.FindWithTag("Player").GetComponent<InventoryManager>();
+        inventoryManager.AddItem(inventoryManager.GetItemByName("Protective Skull"));
+
+        // Display the text from the skull
+        gameObject.GetComponent<subTitleTrigger>().StartDisplay();
+
+        // Remove the item from the world
+        Destroy(gameObject);
+    }
+}
