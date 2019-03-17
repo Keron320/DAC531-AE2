c561ecb02d74e5100312331e42dc00b2d9dd1451
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-03-17 23:10:09 +0000

    Room A added statues
    + statues 3d models
    + Statue looking at the player script

---
+++ Assets/Assets/Statues/LookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+      public GameObject target;
+ 
+    private void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(-90,0,0,Space.World);
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Assets/Statues/Statue_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue_Script : MonoBehaviour
+{
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        enabled = true;
+    }
+
+    // ...and enable it again when it becomes visible.
+    void OnBecameVisible()
+    {
+        enabled = false;
+    }
+}
