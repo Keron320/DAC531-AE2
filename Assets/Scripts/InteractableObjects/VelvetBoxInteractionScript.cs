274feeb291ced79d501c8536a20a7d974be797de
Andrew (LethalsLoaded) <odwalda@gmail.com>
2019-03-24 19:55:54 +0000

    Sharp stick and velvet box

---
+++ Assets/Scripts/InteractableObjects/VelvetBoxInteractionScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelvetBoxInteractionScript : InteractiveObject
+{
+    public override void UseItem()
+    {
+        // Add the item
+        var inventoryManager = GameObject.FindWithTag("Player").GetComponent<InventoryManager>();
+        inventoryManager.AddItem(inventoryManager.GetItemByName("Sharp Stick"));
+    }
+}
