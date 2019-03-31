8de55917f00e999e6e192d03614e14c327fe8b4d
Keron320 <32654913+Keron320@users.noreply.github.com>
2019-03-31 15:58:54 +0100

    Simple endtriggerscript

---
+++ Assets/Scripts/gameEndScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameEndScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool gameWon = false;
+    public bool gameLost = false;
+
+
+    public GameObject win;
+    public GameObject lose;
+
+
+    // To activate in any script of your own
+    // Example you have Dracula health < 0;
+    //GameObject.Find("GoForScripts").GetComponent<gameEndScript>().gameWon = true;
+    // Or "On Trigger enter" Function that just enables this part of the script.
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (gameWon == true)
+        {
+            win.SetActive(true);
+            //DOthat
+        }
+
+        else if (gameLost == true)
+        {
+            lose.SetActive(true);
+            //DOthat
+        }
+
+    }
+}
