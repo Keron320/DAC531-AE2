f7de79bca8b7c46b400001835c34d9ee139f30a0
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-04-10 16:06:07 +0100

    Water Footsteps sound effect

---
+++ Assets/Scripts/FootStepScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepScript : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioClip[] waterClips;
+    public AudioSource audioSource;
+    public bool isInWater;
+    
+    void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        if (isInWater)
+         return waterClips[UnityEngine.Random.Range(0,waterClips.Length)];
+        else
+         return clips[UnityEngine.Random.Range(0,clips.Length)];
+    }
+}
