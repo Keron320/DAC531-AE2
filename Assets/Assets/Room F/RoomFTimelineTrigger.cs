458220a12e8f5df801d3c237afeca83721d075d7
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-04-11 15:43:26 +0100

    Room F Improvements

---
+++ Assets/Assets/Room F/RoomFTimelineTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class RoomFTimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector pD;
+    public PlayableAsset spiderBitingYou;
+    public PlayableAsset spiderDying;
+    bool canBePressed;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            // start timeline
+            pD.Play();
+            StartCoroutine(Wait());
+        }
+    }
+
+    IEnumerator Wait() {
+        {
+            // after 5 seconds pause timeline and allow player to interact
+           yield return new WaitForSeconds(5);
+            //player can interact
+           canBePressed = true;
+           pD.Pause();
+           yield return new WaitForSeconds(2);
+            // disable interacton for player
+           canBePressed = false;
+           pD.Play();
+        
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // chceck for imput from player
+        if (canBePressed)
+        {
+            // if player pressed interaction button change playable asset to spider dying
+            
+        }
+        
+    }
+}
