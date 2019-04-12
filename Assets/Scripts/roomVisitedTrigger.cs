3c71f9162380a012a8c1ab83e82b97d8b3b2d273
Keron320 <32654913+Keron320@users.noreply.github.com>
2019-04-12 23:54:48 +0100

    Rooms visited ( can open door D when all is visited )

---
+++ Assets/Scripts/roomVisitedTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomVisitedTrigger : MonoBehaviour
+{
+    public GameObject roomsVisitedScript;
+
+    public bool iAmRoomA = false;
+    public bool iAmRoomB = false;
+    public bool iAmRoomC = false;
+    public bool iAmRoomE = false;
+    public bool iAmRoomF = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(iAmRoomA == true)
+        roomsVisitedScript.GetComponent<roomsVisited>().roomVisitedA = true;
+
+        if (iAmRoomB == true)
+            roomsVisitedScript.GetComponent<roomsVisited>().roomVisitedB = true;
+
+        if (iAmRoomC == true)
+            roomsVisitedScript.GetComponent<roomsVisited>().roomVisitedC = true;
+
+        if (iAmRoomE == true)
+            roomsVisitedScript.GetComponent<roomsVisited>().roomVisitedE = true;
+
+        if (iAmRoomF == true)
+            roomsVisitedScript.GetComponent<roomsVisited>().roomVisitedF = true;
+    }
+
+}
---
+++ Assets/Scripts/roomsVisited.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomsVisited : MonoBehaviour
+{
+
+    public bool roomVisitedA = false;
+    public bool roomVisitedB = false;
+    public bool roomVisitedC = false;
+    public bool roomVisitedE = false;
+    public bool roomVisitedF = false;
+
+    public GameObject myDoor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(roomVisitedA && roomVisitedB && roomVisitedC  && roomVisitedE && roomVisitedF == true)
+        {
+            // If all conditions are true ( all rooms visited ) enable this doors option to be interactable ( can be used on any doors, first you must disable canInteract though )
+            myDoor.GetComponent<DoorInteractionScript>().canInteract = true;
+        }
+    }
+}
